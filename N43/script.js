cb0158f51ac84ab1c673ab4d1128d3bbec5542f7
Mikhail-Tarasevich <tarasevich.mix@gmail.com>
2019-08-29 17:24:18 +0300

    Добавлены и спозиционированы ракетки и мяч

--- N43/script.js
+++ N43/script.js
@@ -2,13 +2,11 @@
 
 function run() {
     var rocket1 = document.getElementById('rocket1');
-    rocket1.style.top = 100 + 'px'; 
-    rocket1.style.right = 100 + 'px';
+//    rocket1.style.right = 100 + 'px';
     rocket1.style.backgroundColor = '#09A85A';
 
     var rocket2 = document.getElementById('rocket2');
-    rocket2.style.top = 200 + 'px'; 
-    rocket2.style.bottom = 200 + 'px'; 
+    rocket2.style.left = 780 + 'px'; 
     rocket2.style.backgroundColor = '#171392';
 }
 
