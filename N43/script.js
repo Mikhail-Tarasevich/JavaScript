7972f69aa233610e818da666ce86a164448d0f50
Mikhail-Tarasevich <tarasevich.mix@gmail.com>
2019-09-03 17:29:03 +0300

    попытка запрограммировать гол

--- N43/script.js
+++ N43/script.js
@@ -1,20 +1,21 @@
 "use strict";
 
-var rocket1 = null;
-var rocket2 = null;
-var tennis = null;
-var ball = null;
-
-var ROCKET_SPEED = 5;
-
-var BALL_SPEED = 5;
-var BALL_HEIGTH = 50;
-var BALL_RADIUS = BALL_HEIGTH / 2;
-
 function run() {
+    var rocket1 = null;
+    var rocket2 = null;
+    var tennis = null;
+    var ball = null;
+    
+    var ROCKET_SPEED = 5;
+    var BALL_SPEED = 1;
+
     tennis = document.getElementById('tennis');
+    var TENNIS_SIZE_X = tennis.offsetWidth - 2; // размер поля по X равен offsetWidth - 2px на границы
+    var TENNIS_SIZE_Y = tennis.offsetHeight - 2; // размер поля по Y равен offsetHeight - 2px на границы
 
     ball = document.getElementById('ball');
+    var BALL_HEIGTH = ball.offsetHeight;
+    var BALL_RADIUS = BALL_HEIGTH / 2;
     ball.speedV = (Math.random()<0.5)? -1 : 1; // вектор скорости по вертикали (1 и -1)
     ball.speedG = (Math.random()<0.5)? -1 : 1; // вектор скорости по горизонтали (1 и -1)
     
@@ -22,8 +23,11 @@ function run() {
     rocket1.style.backgroundColor = '#09A85A';
     rocket1.speed = 0;
 
+    var ROCKET_WIDTH = rocket1.offsetWidth;
+    var ROCKET_HEIGTH = rocket1.offsetHeight;
+
     rocket2 = document.getElementById('rocket2');
-    rocket2.style.left = 780 + 'px'; 
+    rocket2.style.left = (TENNIS_SIZE_X - ROCKET_WIDTH) + 'px'; 
     rocket2.style.backgroundColor = '#171392';
     rocket2.speed = 0;
 
@@ -100,35 +104,83 @@ function run() {
         draw();
         requestAnimationFrame(measure);
     });
+
+    function draw() {
+        rocket1.style.top = (rocket1.offsetTop + rocket1.speed) + 'px';
+        rocket2.style.top = (rocket2.offsetTop + rocket2.speed) + 'px';
+    
+        var ballX = ball.offsetLeft + BALL_RADIUS;
+        var ballY = ball.offsetTop + BALL_RADIUS;
+        var ballXnew = ballX + (ball.speedG * BALL_SPEED);
+        var ballYnew = ballY + (ball.speedV * BALL_SPEED);
+        var tennisTop = tennis.offsetTop;
+        var tennisHeight = tennis.offsetHeight;
+        var tennisLeft = tennis.offsetLeft;
+        var tennisWidth = tennis.offsetWidth;
+        var speedG = ball.speedG;
+        var speedV = ball.speedV;
+        if (ballYnew<=BALL_RADIUS+1) { // верхняя стенка
+            ballYnew = BALL_RADIUS+1;
+            ball.speedV = -ball.speedV;
+        }
+        else if (ballXnew>=(TENNIS_SIZE_X-ROCKET_WIDTH-BALL_RADIUS-1)) {  // правая стенка
+            if ((ballYnew>rocket2.offsetTop) && (ball.speedV>0)) { // попал в ракетку при движении мяча вниз
+                ballXnew = tennis.offsetWidth-ROCKET_WIDTH-BALL_RADIUS-1;
+                ball.speedG = -ball.speedG;
+            }
+            else if ((ballYnew<(rocket2.offsetTop+ROCKET_HEIGTH)) && (ball.speedV<0)) { // попал в ракетку при движении мяча вверх
+                ballXnew = tennis.offsetWidth-ROCKET_WIDTH-BALL_RADIUS-1;
+                ball.speedG = -ball.speedG;
+            }
+            else { // гол правому игроку
+                BALL_SPEED = 0; 
+                if (((ballYnew+BALL_RADIUS)>=rocket2.offsetTop) && (ballYnew<(rocket2.offsetTop+BALL_RADIUS))) { // попал в верхний край ракетки и отскочил в гол
+                    // устанавливаем мяч сверху, рядом с ракеткой
+                    ballYnew = rocket2.offsetTop-BALL_RADIUS; 
+                    ballXnew = TENNIS_SIZE_X-BALL_RADIUS;
+                }
+                else if (((ballYnew+BALL_RADIUS)<=rocket2.offsetTop+ROCKET_HEIGTH) && (ballYnew<(rocket2.offsetTop+ROCKET_HEIGTH+BALL_RADIUS))) { // попал в нижний край ракетки и отскочил в гол
+                    // устанавливаем мяч снизу, рядом с ракеткой
+                    ballYnew = rocket2.offsetTop+ROCKET_HEIGTH+BALL_RADIUS; 
+                    ballXnew = TENNIS_SIZE_X-BALL_RADIUS;
+                }
+            }
+        }
+        else if (ballXnew<=(ROCKET_WIDTH+1)) {  // левая стенка
+            if ((ballYnew>rocket1.offsetTop) && (ball.speedV>0)) { // попал в ракетку при движении мяча вниз
+                ballXnew = ROCKET_WIDTH + 1;
+                ball.speedG = -ball.speedG;
+            }
+            else if ((ballYnew<(rocket1.offsetTop+ROCKET_HEIGTH)) && (ball.speedV<0)) { // попал в ракетку при движении мяча вверх
+                ballXnew = ROCKET_WIDTH + 1;
+                ball.speedG = -ball.speedG;
+            }
+            else { // гол левому игроку
+                BALL_SPEED = 0; 
+                if (((ballYnew+BALL_RADIUS)>=rocket2.offsetTop) && (ballYnew<(rocket2.offsetTop+BALL_RADIUS))) { // попал в верхний край ракетки и отскочил в гол
+                    // устанавливаем мяч сверху, рядом с ракеткой
+                    ballYnew = rocket2.offsetTop-BALL_RADIUS; 
+                    ballXnew = BALL_RADIUS;
+                }
+                else if (((ballYnew+BALL_RADIUS)<=rocket2.offsetTop+ROCKET_HEIGTH) && (ballYnew<(rocket2.offsetTop+ROCKET_HEIGTH+BALL_RADIUS))) { // попал в нижний край ракетки и отскочил в гол
+                    // устанавливаем мяч снизу, рядом с ракеткой
+                    ballYnew = rocket2.offsetTop+ROCKET_HEIGTH+BALL_RADIUS; 
+                    ballXnew = BALL_RADIUS;
+                }
+            }
+        }
+        else if (ballYnew>=(TENNIS_SIZE_Y-BALL_HEIGTH-1)) {  // нижняя стенка
+            ballYnew = TENNIS_SIZE_Y-BALL_HEIGTH-1;
+            ball.speedV = -ball.speedV;
+        };
+
+        if ((ballXnew>(TENNIS_SIZE_X-BALL_HEIGTH)) || (ballYnew>(TENNIS_SIZE_Y-BALL_HEIGTH)) || (ballXnew<BALL_HEIGTH) || (ballYnew<BALL_HEIGTH)) {
+            console.log(ballXnew + ", " + ballXnew);
+        }
+
+        ball.style.left = (ballXnew - BALL_RADIUS) + 'px';
+        ball.style.top = (ballYnew - BALL_RADIUS) + 'px';
+    };
 };
-
-function draw() {
-    rocket1.style.top = (rocket1.offsetTop + rocket1.speed) + 'px';
-    rocket2.style.top = (rocket2.offsetTop + rocket2.speed) + 'px';
-
-    var ballX = ball.offsetLeft + BALL_RADIUS;
-    var ballY = ball.offsetTop + BALL_RADIUS;
-    var ballXnew = ballX + (ball.speedG * BALL_SPEED) - BALL_RADIUS;
-    var ballYnew = ballY + (ball.speedV * BALL_SPEED) - BALL_RADIUS;
-    if (ballXnew<=(tennis.offsetTop)) {
-        var ballXnew = tennis.offsetTop;
-        ball.speedV = -ball.speedV;
-    };
-    if (ballXnew>=(tennis.offsetHeight+BALL_HEIGTH)) {
-        var ballXnew = tennis.offsetHeight-BALL_HEIGTH;
-        ball.speedV = -ball.speedV;
-    };
-    if (ballYnew<=(tennis.offsetLeft)) {
-        var ballYnew = tennis.offsetLeft;
-        ball.speedG = -ball.speedG;
-    };
-    if (ballYnew>=(tennis.offsetWidth+BALL_HEIGTH)) {
-        var ballYnew = tennis.offsetWidth-BALL_HEIGTH;
-        ball.speedG = -ball.speedG;
-    };
-    ball.style.left = ballXnew + 'px';
-    ball.style.top = ballYnew + 'px';
-};
-
 
 run();
