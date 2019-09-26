c159974b3c54635c80fd3f558e388a30b4e5b8f6
Mikhail-Tarasevich <tarasevich.mix@gmail.com>
2019-09-26 10:41:54 +0300

    N60 v1.0.0

---
+++ N60/script.js
@@ -0,0 +1,138 @@
+var formDef1="";
+var formDef2="";
+
+function run() {
+  var ajaxHandlerScript1="FormDef1.json";
+  var ajaxHandlerScript2="FormDef2.json";
+
+  $.ajax(ajaxHandlerScript1, { type:'GET', dataType:'json', success:readFormDef1, error:errorHandler } );
+  $.ajax(ajaxHandlerScript2, { type:'GET', dataType:'json', success:readFormDef2, error:errorHandler } );
+  
+  setElements(document.forms.form1,formDef1);
+  setElements(document.forms.form2,formDef2);
+}
+
+function setElements(form,elem) {
+  var res = "";
+  for (var i=0; i<=elem.length; i++) {
+    var j = elem[i];
+    if (j!=undefined) {
+
+      var div = document.createElement('div');
+
+      switch (j.kind) {
+      case 'number':
+        var element1 = document.createElement("label");
+        element1.innerText=j.label;
+        div.appendChild(element1); 
+
+        var element2 = document.createElement("input");
+        element2.name=j.name;
+          element2.type='number';
+        div.appendChild(element2); 
+
+        break;
+      case 'longtext':
+        var element1 = document.createElement("label");
+        element1.innerText=j.label;
+        div.appendChild(element1); 
+
+        var element2 = document.createElement("input");
+        element2.name=j.name;
+        element2.style.width='453px';
+        div.appendChild(element2); 
+
+        break;
+      case 'shorttext':
+        var element1 = document.createElement("label");
+        element1.innerText=j.label;
+        div.appendChild(element1); 
+
+        var element2 = document.createElement("input");
+        element2.name=j.name;
+        element2.style.width='200px';
+        div.appendChild(element2); 
+
+        break;
+      case 'submit':
+        var element2 = document.createElement("button");
+        element2.innerHTML=j.label;
+        element2.type='button';
+        div.appendChild(element2); 
+
+        break;
+      case 'memo':
+        var element1 = document.createElement("label");
+        element1.innerText=j.label;
+        div.appendChild(element1); 
+
+        var element2 = document.createElement("textarea");
+        element2.name=j.name;
+        element2.cols='80';
+        element2.rows='20';
+        div.appendChild(element2); 
+
+        break;
+      case 'combo':
+        var element1 = document.createElement("label");
+        element1.innerText=j.label;
+        div.appendChild(element1); 
+
+        var element2 = document.createElement("select");
+        var opt = null;
+        for(r=0; r<=j.variants.length; r++) {
+          var txt = j.variants[r];
+          if (txt!=undefined) {
+            opt = document.createElement('option');
+            var t = document.createTextNode(txt['text']);
+            opt.value = txt['text'];
+            opt.appendChild(t);
+            element2.appendChild(opt);
+          }
+        }
+
+        div.appendChild(element2); 
+
+        break;
+      case 'radio':
+        var element1 = document.createElement("label");
+        element1.innerText=j.label;
+        div.appendChild(element1); 
+        var checked = true;
+
+        for(r=0; r<=j.variants.length; r++) {
+          var txt = j.variants[r];
+          if (txt!=undefined) {
+            var element2 = document.createElement("input");
+            element2.name = j.name;
+            element2.type='radio';
+            element2.setAttribute('value', txt['text']);
+            if (checked) {
+              element2.setAttribute('checked', 'checked');
+              checked = false;
+            };
+            div.appendChild(element2); 
+            div.innerHTML += "<span>" + txt['text'] + "</span>";  
+          }
+        }
+        break;
+      }
+
+      form.appendChild(div); 
+      document.body.appendChild(form);
+    }
+  }
+}
+
+function readFormDef1(data) {
+  formDef1 = data;
+}
+
+function readFormDef2(data) {
+  formDef2 = data;
+}
+
+function errorHandler(jqXHR,statusStr,errorStr) {
+  alert(statusStr+' '+errorStr);
+}
+
