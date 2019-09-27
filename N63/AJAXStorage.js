5694b71eb9d60a5066e4ebeba8d5e455b3de3203
Mikhail-Tarasevich <tarasevich.mix@gmail.com>
2019-09-27 16:49:16 +0300

    N63 v1.0.0

--- N63/AJAXStorage.js
+++ N63/AJAXStorage.js
@@ -1,64 +1,133 @@
 var ajaxHandlerScript="https://fe.it-academy.by/AjaxStringStorage2.php";
-var updatePassword;
 var stringName='TARASEVICH_N63';
 
-function storeInfo() {
-    updatePassword=Math.random();
-    $.ajax( {
-            url : ajaxHandlerScript, type : 'POST', cache : false, dataType:'json',
-            data : { f : 'LOCKGET', n : stringName, p : updatePassword },
-            success : lockGetReady, error : errorHandler
+class AJAXStorage {
+    constructor(storename) {
+        this.updatePassword;
+        this.storeName = stringName + '_' + storename; 
+        this.storeValue = {};
+        this.isStoreInit = false;
+        this.restoreInfo();
+    };
+    
+    addValue(key,value) {
+        if (this.IsStorageInit()) {
+            this.storeValue[key] = value;
+            this.storeInfo();
         }
-    );
-}
+    }
+    
+    getValue(key) {
+        if (this.IsStorageInit()) {
+            this.restoreInfo();
+            return this.storeValue[key];
+        }
+    }
+    
+    deleteValue(key) {
+        if (this.IsStorageInit()) {
+            if (key in this.storeValue) {
+                delete this.storeValue[key];
+                this.storeInfo();
+                return true;
+              }
+              else {
+                return false;
+              }
+        }
+    }
+    
+    getKeys() {
+        if (this.IsStorageInit()) {
+            this.restoreInfo();
+            return Object.keys(this.storeValue)
+        }
+        else {
+            return null;
+        }
+    }
 
-function lockGetReady(callresult, info) {
-    if ( callresult.error!=undefined )
-        alert(callresult.error);
-    else {
-        // нам всё равно, что было прочитано -
-        // всё равно перезаписываем
-        var info={
-            name : document.getElementById('IName').value,
-            age : document.getElementById('IAge').value
-        };
+    insertInfo() {
+        $.ajax( {
+            url : ajaxHandlerScript, type : 'POST', cache : false, dataType:'json',
+            data : { f : 'INSERT', n : this.storeName, v : JSON.stringify(this.storeValue) },
+            success : (callresult)=>this.updateReady(callresult), error : (jqXHR,statusStr,errorStr)=>this.errorHandler(jqXHR,statusStr,errorStr)
+        }
+        );
+    }
+    
+    storeInfo() {
+        this.updatePassword=Math.random();
         $.ajax( {
                 url : ajaxHandlerScript, type : 'POST', cache : false, dataType:'json',
-                data : { f : 'UPDATE', n : stringName, v : JSON.stringify(info), p : updatePassword },
-                success : updateReady, error : errorHandler
+                data : { f : 'LOCKGET', n : this.storeName, p : this.updatePassword },
+                success : (callresult)=>this.lockGetReady(callresult), error : (jqXHR,statusStr,errorStr)=>this.errorHandler(jqXHR,statusStr,errorStr)
             }
         );
     }
-}
-
-function updateReady(callresult) {
-    if ( callresult.error!=undefined )
-        alert(callresult.error);
-}
-
-function restoreInfo() {
-    $.ajax(
-        {
-            url : ajaxHandlerScript, type : 'POST', cache : false, dataType:'json',
-            data : { f : 'READ', n : stringName },
-            success : readReady, error : errorHandler
+    
+    lockGetReady(callresult) {
+        if ( callresult.error!=undefined )
+            alert("lockGetReady: " + callresult.error);
+        else {
+            this.postInfo();
         }
-    );
-}
-
-function readReady(callresult) {
-    if ( callresult.error!=undefined )
-        alert(callresult.error);
-    else if ( callresult.result!="" ) {
-        var info=JSON.parse(callresult.result);
-        document.getElementById('IName').value=info.name;
-        document.getElementById('IAge').value=info.age;
     }
+    
+    postInfo() {
+        $.ajax( {
+                url : ajaxHandlerScript, type : 'POST', cache : false, dataType:'json',
+                data : { f : 'UPDATE', n : this.storeName, v : JSON.stringify(this.storeValue), p : this.updatePassword },
+                success : (callresult)=>this.updateReady(callresult), error : (jqXHR,statusStr,errorStr)=>this.errorHandler(jqXHR,statusStr,errorStr)
+            }
+        );
+    }
+    
+    updateReady(callresult) {
+        if ( callresult.error!=undefined )
+            alert("updateReady: "+callresult.error);
+    }
+    
+    restoreInfo() {
+        $.ajax(
+            {
+                url : ajaxHandlerScript, type : 'POST', cache : false, dataType:'json',
+                data : { f : 'READ', n : this.storeName },
+                success : (callresult)=>this.readReady(callresult), error : (jqXHR,statusStr,errorStr)=>this.errorHandler(jqXHR,statusStr,errorStr)
+            }
+        );
+    }
+    
+    readReady(callresult) {
+        this.isStoreInit = true;
+        if ( callresult.error!=undefined ) {
+            alert("readReady: "+callresult.error);
+            this.isStoreInit = false;
+        }
+        else if ( callresult.result!="" ) {
+            this.storeValue=JSON.parse(callresult.result);
+        }
+    }
+    
+    errorHandler(jqXHR,statusStr,errorStr) {
+        alert("errorHandler: "+statusStr+' '+errorStr);
+    }
+
+    IsStorageInit() {
+        if (!this.isStoreInit) {
+            alert("Данные еще не загружены - попробуйте позже")
+            return false;
+        }
+        else {
+            return true;
+        };
+    }
+
 }
 
-function errorHandler(jqXHR,statusStr,errorStr) {
-    alert(statusStr+' '+errorStr);
-}
 
-restoreInfo();
+
+
+
+
 
