7878fff498815de398bc2cd2993c08663d66e611
Mikhail-Tarasevich <tarasevich.mix@gmail.com>
2019-10-16 17:48:27 +0300

    RB199

---
+++ ishop/Shop.js
@@ -0,0 +1,26 @@
+﻿var Shop = React.createClass({
+
+  displayName: 'Shop',
+
+  propTypes: {
+    shopname: React.PropTypes.string.isRequired,
+    shopgoods: React.PropTypes.array.isRequired,
+  },
+
+  render: function() {
+
+    var shopgoods=this.props.shopgoods.map( v =>
+        React.DOM.div({key:v.code,className:'Goods'},
+          React.DOM.span({className:'Name'},v.name),
+          React.DOM.span({className:'Price'},v.price),
+          React.DOM.span({className:'Fotos'},v.fotos),
+          React.DOM.span({className:'Count'},v.count),
+        )
+      );
+    return React.DOM.div( {className:'Shop'}, 
+      React.DOM.div( {className:'Name'}, this.props.shopname ),
+      React.DOM.div( {className:'Goods'}, shopgoods ),
+    );
+  },
+
+});
