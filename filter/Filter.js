f59125e6aebc9ec0da6a797608f39fb9a3e26cea
Mikhail-Tarasevich <tarasevich.mix@gmail.com>
2019-10-25 17:22:53 +0300

    filter v1.1.0

--- filter/Filter.js
+++ filter/Filter.js
@@ -5,31 +5,57 @@ var Filter = React.createClass({
         strList: React.PropTypes.arrayOf(String),
     },
 
-    btnReset: function() {
-//        this.setState( (prevState, props) => { return {cnt:prevState.cnt+1}; } );
-        console.log("btnReset")
+    getInitialState: function() {
+        return { 
+            checkbox: false,
+            strListPrev: this.props.strList, // предыдущее состояние списка
+            strListS: this.props.strList,  // ntreott состояние списка
+        };
+      },
+
+    btnReset: function(EO) {
+        this.setState( (prevState, props) => { 
+            var newArr = props.strList;
+            return {checkbox: false, strListS: newArr, strListPrev: newArr}; 
+        } );
     },
 
-    chbSort: function() {
-        console.log("chbSort")
+    chbSort: function(EO) {
+        var checked = EO.target.checked;
+        this.setState( (prevState, props) => { 
+            var prevArr = [];
+            var newArr = [];
+            if (checked) {
+                prevArr = prevState.strListS.slice();; 
+                newArr = prevState.strListS.sort();;
+            }
+            else {
+                prevArr = prevState.strListPrev;
+                newArr = prevState.strListPrev;
+            }
+            return {checkbox: checked, strListS: newArr, strListPrev: prevArr}; 
+        } );
     },
 
     inpChange: function(EO) {
         var strStart = EO.target.value;
-        console.log("inpChange " + strStart);
-        //     this.setState( (prevState, props) => { return {cnt:prevState.cnt+1}; } );
-        // v.startsWith(EO.target.value)?v:""
-        // this.props.strList.map(v => "1")
-        // var newArr = this.props.strList.filter(v => v.startsWith(strStart));
         this.setState( (prevState, props) => { 
-            return {strList: this.props.strList.push("123")}; 
+            var newArr = [];
+            if (strStart=="") {
+                newArr = props.strList;
+            }
+            else {
+                newArr = prevState.strListS.filter(v => v.indexOf(strStart)>-1);
+            }
+            return {strListS: newArr}; 
         } );
     },
 
     render: function() {
         console.log("render");
 
-        var list = this.props.strList.map( v => React.DOM.div({key:v,className:'List'}, React.DOM.span({className:'Text'},v),));
+        var list = this.state.strListS.map( v => React.DOM.div({key:v,className:'List'}, React.DOM.span({className:'Text'},v),));
+        //var checkbox = (this.state.checkbox)?;
         
         return React.DOM.div( {className:'Filter'}, 
         React.DOM.input({type:'checkbox',name:'isSort',onClick:this.chbSort}),
