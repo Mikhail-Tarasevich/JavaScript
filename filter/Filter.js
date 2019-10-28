55b07c874fe2daff8b1a99f5f643d90515d469c4
Mikhail-Tarasevich <tarasevich.mix@gmail.com>
2019-10-28 20:02:22 +0300

    filter v1.2.0

--- filter/Filter.js
+++ filter/Filter.js
@@ -7,16 +7,18 @@ var Filter = React.createClass({
 
     getInitialState: function() {
         return { 
-            checkbox: false,
+            textvalue: "", // содержимое поля для ввода текстового фильтра
+            isReset: false, // флаг нажатия кнопки Сброс
+            checkbox: false, // состояние чекбокса
             strListPrev: this.props.strList, // предыдущее состояние списка
-            strListS: this.props.strList,  // ntreott состояние списка
+            strListS: this.props.strList,  // текущее состояние списка
         };
       },
 
     btnReset: function(EO) {
         this.setState( (prevState, props) => { 
             var newArr = props.strList;
-            return {checkbox: false, strListS: newArr, strListPrev: newArr}; 
+            return {checkbox: false, strListS: newArr, strListPrev: newArr, isReset: true, textvalue: ""}; 
         } );
     },
 
@@ -47,19 +49,16 @@ var Filter = React.createClass({
             else {
                 newArr = prevState.strListS.filter(v => v.indexOf(strStart)>-1);
             }
-            return {strListS: newArr}; 
+            return {strListS: newArr, textvalue: strStart}; 
         } );
     },
 
     render: function() {
-        console.log("render");
-
         var list = this.state.strListS.map( v => React.DOM.div({key:v,className:'List'}, React.DOM.span({className:'Text'},v),));
-        //var checkbox = (this.state.checkbox)?;
-        
+
         return React.DOM.div( {className:'Filter'}, 
-        React.DOM.input({type:'checkbox',name:'isSort',onClick:this.chbSort}),
-        React.DOM.input({type:'text',name:'filtertext',onChange:this.inpChange}),
+        React.DOM.input({type:'checkbox',name:'isSort',onClick:this.chbSort, onChange:this.chbSort, checked:(this.state.checkbox), value: this.state.checkbox}),
+        React.DOM.input({type:'text',name:'filtertext',onChange:this.inpChange, value:this.state.textvalue}),
         React.DOM.input({type:'button',value:'Сброс',onClick:this.btnReset}),
         list,
         );
