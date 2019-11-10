1791eebe8f389b074d9a263bc8e1670b2864652e
Mikhail-Tarasevich <tarasevich.mix@gmail.com>
2019-11-10 18:25:40 +0300

    ishop2 v1.2.1

--- ishop2/Good.js
+++ ishop2/Good.js
@@ -3,25 +3,44 @@ var Good = React.createClass({
   displayName: 'Good',
 
   propTypes: {
+    url: React.PropTypes.string,
     name: React.PropTypes.string,
-    count: React.PropTypes.number,
-    price: React.PropTypes.number,
+    count: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.number
+    ]),
+    price: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.number
+    ]),
   },
 
   getInitialState: function() {
     return {
+      id: this.props.id,
+      url: this.props.url,
       name: this.props.name,
       count: this.props.count,
       price: this.props.price,
     };
   },
 
+  actionDelete: function() {
+    console.log('actionDelete: ' + this.props.id);
+  },
+
   render: function() {
     var goodline  = Object.assign({}, this.state);
 
-    return  React.DOM.table({className: "MyClassName"}, 
+    return  React.DOM.table({className: "GoodLine"}, 
       React.DOM.thead(null, 
-        React.DOM.tr(null, React.DOM.td({className: "td1"}, goodline.name), React.DOM.td({className: "td2"}, goodline.count), React.DOM.td({className: "td3"}, goodline.price)),
+        React.DOM.tr(null,
+          React.DOM.td({className: "tdName"}, goodline.name), 
+          React.DOM.td({className: "tdPrice"}, goodline.price), 
+          React.DOM.td({className: "tdURL"}, goodline.url), 
+          React.DOM.td({className: "tdCount"}, goodline.count), 
+          React.DOM.td({className: "tdControl"}, React.DOM.input({type:'button',value:'Delete',onClick:this.actionDelete}))
+        ),
       )
     );
     
