40281bb77cb7d4f1bb78f777565aabeef2cadc52
Mikhail-Tarasevich <tarasevich.mix@gmail.com>
2019-12-10 16:09:25 +0300

    Rainbow: 1 color

---
+++ RainbowFrame/App.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './components/RainbowFrame';
+
+// let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+// <ColorFrame color="red">
+// array1.forEach(element => console.log(element));
+let colors = 'red';
+
+ReactDOM.render(
+    <RainbowFrame color={colors}>
+      Hello!
+    </RainbowFrame>
+  , document.getElementById('container') 
+);
+
---
+++ RainbowFrame/components/RainbowFrame.js
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class RainbowFrame extends React.Component {
+
+  static propTypes = {
+    color: PropTypes.string.isRequired,
+    //colors: PropTypes.array.isRequired,
+  };
+  
+  render() {
+//    let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
+    return (
+      <div style={{border:"solid 5px "+this.props.color,padding:"10px"}}>
+        {this.props.children}
+      </div>
+    );
+  }
+
+}
+
+export default RainbowFrame;
