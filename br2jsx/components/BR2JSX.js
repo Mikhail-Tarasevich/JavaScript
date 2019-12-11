71ff0b0b5aa2f3979dd881c1458eca1b178e81ed
Mikhail-Tarasevich <tarasevich.mix@gmail.com>
2019-12-11 13:57:14 +0300

    BR2JSX: v1

--- br2jsx/components/BR2JSX.js
+++ br2jsx/components/BR2JSX.js
@@ -4,26 +4,24 @@ import PropTypes from 'prop-types';
 class BR2JSX extends React.Component {
 
   static propTypes = {
-    colors: PropTypes.array.isRequired,
+    text: PropTypes.string.isRequired,
   };
   
   render() {
-    let colors = ['red','orange', 'yellow','green', '#00BFFF', 'blue', 'purple'];
-    let res = null;
-    if (this.props.colors.length==1) {
-      res = <div style={{border:"solid 5px "+this.props.colors[0],padding:"10px"}}>
-              {this.props.children}
-            </div>
-    }
-    else {
-      res = <div style={{border:"solid 5px "+this.props.colors[this.props.colors.length-1],padding:"10px"}}>
-              <BR2JSX colors={this.props.colors.slice(0, this.props.colors.length-1)}>
-                {this.props.children}
-              </BR2JSX>
-            </div>
-    }
+    let match = [];
+    let i = 0;
 
-    return res;
+    this.props.text.split(/<br\s*\/*>/).forEach(function(line) {
+      i++;
+      match.push(<span key={i.toString()}>{line}</span>);
+      match.push(<br/>);
+    });
+
+    return (
+            <div>
+              {match}
+            </div>
+    )
   }
 
 }
