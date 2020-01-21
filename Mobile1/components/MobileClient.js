d32bd3d3498b458248b22b48f183a46fdff9165a
Mikhail-Tarasevich <tarasevich.mix@gmail.com>
2020-01-21 10:59:06 +0300

    mobile1: добавлена смена статуса

--- Mobile1/components/MobileClient.js
+++ Mobile1/components/MobileClient.js
@@ -34,11 +34,24 @@ class MobileClient extends React.PureComponent {
   componentDidMount = () => {
     MobileEvents.addListener('EditClient',this.clientEditProcess);
     MobileEvents.addListener('DeleteClient',this.clientDeleteProcess);
+    MobileEvents.addListener('StatusClient',this.clientSetStatus);
   };
 
   componentWillUnmount = () => {
     MobileEvents.removeListener('EditClient',this.clientEditProcess);
     MobileEvents.removeListener('DeleteClient',this.clientDeleteProcess);
+    MobileEvents.removeListener('StatusClient',this.clientSetStatus);
+  };
+
+  clientSetStatus = (cid) => {
+    console.log("Status client id="+cid);
+
+    var client1 = Object.assign({}, this.state);
+    client1.info.status = (client1.info.status=='active') ? 'blocked' : 'active';
+//    var client2 = client1.set('info', client1.get('info'))
+
+//    this.setState({info: client2.get('info')})
+      this.setState({info: client1.info})
   };
 
   clientEditProcess = (cid) => {
@@ -72,6 +85,10 @@ var client2 = client1.set('info', client1.get('info'))
     MobileEvents.emit('EditClient', this.state.info.id);
   };
 
+  clientChangeStatus = () => {
+    MobileEvents.emit('StatusClient', this.state.info.id);
+  };
+  
   componentWillReceiveProps = (newProps) => {
     console.log("MobileClient id="+this.props.info.id+" componentWillReceiveProps");
     this.setState({info:newProps.info});
@@ -95,8 +112,8 @@ var client2 = client1.set('info', client1.get('info'))
               <td align='left'><div><input className='cellText' type="text" readOnly={!client.get('info').editmode} defaultValue={client.get('info').name_f}/></div></td>
               <td align='left'><div><input className='cellText' type="text" readOnly={!client.get('info').editmode} defaultValue={client.get('info').name_n}/></div></td>
               <td align='left'><div><input className='cellText' type="text" readOnly={!client.get('info').editmode} defaultValue={client.get('info').name_o}/></div></td>
-              <td className='MobileClientStatus' align='center' width='70px' bgcolor={clientStatusBGColor}>{client.get('info').status}</td>
-              <td className='MobileClientBalance' align='center' width='70px'>{client.get('info').balance}</td>
+              <td className='MobileClientStatus' align='center' width='70px' bgcolor={clientStatusBGColor} onClick={this.clientChangeStatus}>{client.get('info').status}</td>
+              <td align='center'><div><input className='cellNumber' type="text" readOnly={!client.get('info').editmode} defaultValue={client.get('info').balance}/></div></td>
               <td className='MobileClientEdit' align='center' width='100px'><input type="button" value="Редактировать" onClick={this.clientEdit}/></td>
               <td className='MobileClientDelete' align='center' width='100px'><input type="button" value="Удалить" onClick={this.clientDelete}/></td>
 
