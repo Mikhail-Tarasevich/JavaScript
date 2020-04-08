2fd067a935650db12a10f3c9a377a40a577d24d1
Mikhail-Tarasevich <tarasevich.mix@gmail.com>
2020-04-08 14:52:42 +0300

    ishop3: добавлен контроль заполнения полей

--- ishop3/components/GoodEdit.js
+++ ishop3/components/GoodEdit.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './GoodEdit.css';
+import u from 'updeep'; 
 
 class GoodEdit extends React.PureComponent {
     static propTypes = {
@@ -23,7 +24,76 @@ class GoodEdit extends React.PureComponent {
       name: this.props.name,
       count: this.props.count,
       price: this.props.price,
+      errors: {name: "", count: "", price: "", url: ""},
     };
+
+    // обработчик изменений name --- Начало кода
+    name_Ref = null;
+
+    getName = (ref) => {
+      this.name_Ref=ref;
+    };
+
+    onChangeName = () => {
+      var errorName = this.name_Ref.value=='' ? "Не указано наименование товара" : "";
+      if (this.state.errors.name!=errorName) {
+        var errors1 = Object.assign({}, this.state.errors);
+        var errors2 = u({'name': errorName}, errors1);
+        this.setState({errors: errors2});
+      }
+    }
+    // обработчик изменений name --- Конец кода
+
+    // обработчик изменений price --- Начало кода
+    price_Ref = null;
+
+    getPrice = (ref) => {
+      this.price_Ref=ref;
+    };
+
+    onChangePrice = () => {
+      var errorprice = this.price_Ref.value=='' ? "Не указана цена товара" : "";
+      if (this.state.errors.price!=errorprice) {
+        var errors1 = Object.assign({}, this.state.errors);
+        var errors2 = u({'price': errorprice}, errors1);
+        this.setState({errors: errors2});
+      }
+    }
+    // обработчик изменений price --- Конец кода
+
+    // обработчик изменений url --- Начало кода
+    url_Ref = null;
+
+    getUrl = (ref) => {
+      this.url_Ref=ref;
+    };
+
+    onChangeUrl = () => {
+      var errorurl = this.url_Ref.value=='' ? "Не указана ссылка на товар" : "";
+      if (this.state.errors.url!=errorurl) {
+        var errors1 = Object.assign({}, this.state.errors);
+        var errors2 = u({'url': errorurl}, errors1);
+        this.setState({errors: errors2});
+      }
+    }
+    // обработчик изменений url --- Конец кода
+
+    // обработчик изменений count --- Начало кода
+    count_Ref = null;
+
+    getCount = (ref) => {
+      this.count_Ref=ref;
+    };
+
+    onChangeCount = () => {
+      var errorcount = this.count_Ref.value=='' ? "Не указано количество товара" : "";
+      if (this.state.errors.count!=errorcount) {
+        var errors1 = Object.assign({}, this.state.errors);
+        var errors2 = u({'count': errorcount}, errors1);
+        this.setState({errors: errors2});
+      }
+    }
+    // обработчик изменений count --- Конец кода
 
     render() {
         console.log("render GoodEdit "+this.props.id);
@@ -39,12 +109,12 @@ class GoodEdit extends React.PureComponent {
 
             <table className='GoodTable'>
               <thead>
-                <tr><td><div>ID: {this.state.id}</div></td></tr>
-                <tr><td><div><input className='cellText' size='85' type="text" defaultValue={this.state.name}/></div></td></tr>
-                <tr><td><div><input className='cellText' size='5' type="text" defaultValue={this.state.price}/></div></td></tr>
-                <tr><td><div><input className='cellText' size='20' type="text" defaultValue={this.state.url}/></div></td></tr>
-                <tr><td><div><input className='cellText' size='5' type="text" defaultValue={this.state.count}/></div></td></tr>
-                <tr><td className='GoodEditButtons' width='100px'>{buttons}</td></tr>
+                <tr><td>ID:</td><td><div>{this.state.id}</div></td></tr>
+                <tr><td>Name:</td><td><div><input className='cellText' size='85' type="text" defaultValue={this.state.name} ref={this.getName} onChange={this.onChangeName}/></div></td><td><span className='errorText'>{this.state.errors.name}</span></td></tr>
+                <tr><td>Price:</td><td><div><input className='cellText' size='5' type="text" defaultValue={this.state.price} ref={this.getPrice} onChange={this.onChangePrice}/></div></td><td><span className='errorText'>{this.state.errors.price}</span></td></tr>
+                <tr><td>URL:</td><td><div><input className='cellText' size='20' type="text" defaultValue={this.state.url} ref={this.getUrl} onChange={this.onChangeUrl}/></div></td><td><span className='errorText'>{this.state.errors.url}</span></td></tr>
+                <tr><td>Quantity:</td><td><div><input className='cellText' size='5' type="text" defaultValue={this.state.count} ref={this.getCount} onChange={this.onChangeCount}/></div></td><td><span className='errorText'>{this.state.errors.count}</span></td></tr>
+                <tr><td></td><td className='GoodEditButtons' width='100px'>{buttons}</td></tr>
               </thead>
             </table>
           </div>
